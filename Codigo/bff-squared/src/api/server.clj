f968284f1cdc8a8ed1ccbf791fd06de1601a29a3
Marlon Henrique <silvamarlon87@gmail.com>
2022-09-11 11:34:18 -0300

    Componente API (Server)

--- Codigo/bff-squared/src/api/server.clj
+++ Codigo/bff-squared/src/api/server.clj
@@ -3,6 +3,7 @@
   (:require [io.pedestal.http :as http]
             [com.walmartlabs.lacinia.pedestal2 :as lp]
             [com.walmartlabs.lacinia.schema :as schema]
+            [com.walmartlabs.lacinia.pedestal.internal :as internal]
             [com.walmartlabs.lacinia.util :as util]
             [clojure.edn :as edn]
             [api.resolver :as resolver]
@@ -10,14 +11,38 @@
 
 (defn load-definition [] (edn/read-string (slurp "./resources/definition.edn")))
 
-(def schemas
-  (let [definition (load-definition)]
-    (->> diplomat/make-request
-         (resolver/definition->resolvers definition)
-         (util/attach-resolvers (:graphql definition))
-         (schema/compile))))
+(defn create-service
+  [compiled-schema options]
+  (let [{:keys [api-path ide-path port host env cors]
+         :or {api-path "/api"
+              ide-path "/graphiql"
+              port 8080
+              host "localhost"
+              env :dev}} options
+        asset-path "/assets/graphiql"
+        interceptors (lp/default-interceptors compiled-schema nil options)
+        graphiql-handler (fn [_] (internal/graphiql-response api-path "/ws" asset-path {} nil))
+        routes (into #{[api-path :post interceptors :route-name ::graphql-api]
+                       [ide-path :get graphiql-handler :route-name ::graphiql-ide]}
+                     (lp/graphiql-asset-routes asset-path))]
+    (cond-> {:env env
+             ::http/routes routes
+             ::http/port port
+             ::http/host host
+             ::http/type :jetty
+             ::http/join? false}
+      (some? cors) (assoc ::http/allowed-origins cors)
+      (= env :dev) (assoc ::http/secure-headers nil))))
 
-(def service (lp/default-service schemas nil))
+(def new-service
+  (let [{:keys [graphql config] :as definition} (load-definition)
+        schemas (->> diplomat/make-request
+                     (resolver/definition->resolvers definition)
+                     (util/attach-resolvers graphql)
+                     (schema/compile))]
+    [schemas config]))
+
+(def service (apply create-service new-service))
 
 ;; This is an adapted service map, that can be started and stopped.
 ;; From the REPL you can call http/start and http/stop on this service:
