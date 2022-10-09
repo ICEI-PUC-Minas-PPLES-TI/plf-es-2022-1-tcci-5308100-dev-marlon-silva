cd53a993cdc7ebd56cc71a13560710e1b31689ae
Marlon Henrique <silvamarlon87@gmail.com>
2022-10-09 18:35:57 -0300

    Tela de Mutations

--- Codigo/bff-squared/src/manager/views/mutations.cljs
+++ Codigo/bff-squared/src/manager/views/mutations.cljs
@@ -1,12 +1,44 @@
 (ns manager.views.mutations
   (:require [re-frame.core :as rf]
             [re-com.core :as rc :refer [at]]
-            [manager.routes :as routes]))
+            [manager.components.list :as list]
+            [manager.routes :as routes]
+            [manager.components.form :as form]))
+
+(defn arg-form [path]
+  ^{:key (last path)}
+  [form/subform
+   [form/subname-select-input (last path) "Name:" [:request] (butlast path)]
+   [form/composite-select-input :type "Type:" [:inputs] path
+    ^{:key :delete}
+    [:span.input-group-btn
+     [form/button {:style :warning :label "Delete"}
+      :delete-resource-property (butlast path) (last path)]]]])
+
+(defn form []
+  (let [base-path [:data :args]
+        args (map first (get-in @(rf/subscribe [:get-resource]) base-path {}))]
+    [form/scaffold
+     [form/header]
+     [form/inputs nil
+      [form/name-input "Mutation"]
+      [form/text-input :description "Description:" form/root]
+      [form/composite-select-input :type "Type:" [:types] form/root]
+      [form/single-select-input :resolve "Source:" [:sources] form/root]]
+     [form/row :middle
+      [:h4 [:span.page-header "Args:"]]
+      [form/button {:style :info :label "Add"} :new-resource-property :args]]
+     [form/subform-list :sm (map #(arg-form (conj base-path %)) (sort args))]
+     [form/footer]]))
 
 (defn mutations-panel []
   [rc/h-box :src (at)
-   :justify :around
-   :children [[:span "mutations list"]
-              [:span "mutations form"]]])
+   :padding "3vh 5em 0 5em"
+   :gap "5em"
+   :justify :center
+   :children [[rc/box :size "auto"
+               :width "36.4%"
+               :child [list/single-resource]]
+              [form]]])
 
-(defmethod routes/panels :mutations-panel [] [mutations-panel])
+(defmethod routes/panels :mutations [] [mutations-panel])
